180813dd00d8400c82a333cd35fedc62896dee9c
liwenbo <694450321@qq.com>
2023-03-01 10:08:26 +0800

    make 4 pairs: `four cameras` and `eight image paths` in one sample line

--- dataset/make_pair.py
+++ dataset/make_pair.py
@@ -13,15 +13,14 @@ class MakePair:
     def __init__(self, mode='train', source='generate'):
         version = 'v7'
         base_path = f'/home/data/lwb/data/dybev/{version}'
-        self.src_path = f'{base_path}/{mode}/{source}'
-        self.pair_dir = self.src_path + '/pair'
-        if os.path.exists(self.pair_dir):
-            shutil.rmtree(self.pair_dir)
-        os.makedirs(self.pair_dir)
+        src_path = f'{base_path}/{mode}/{source}'
+        self.pair_dir = src_path + '/pair'
+        # if os.path.exists(self.pair_dir):
+            # shutil.rmtree(self.pair_dir)
+        os.makedirs(self.pair_dir, exist_ok=True)
         self.cam_list = ['front', 'back', 'left', 'right']
-        with open(self.src_path + f'/name_list.txt') as f:
+        with open(src_path + f'/name_list.txt') as f:
             self.name_list = f.readlines()
-        self.sv_txt_name_fblr = 'all_fblr_pair_names_list.txt'
         self.sv_txt_name_suff = 'pair_names_list.txt'
         np.random.seed(1)
         self.pert_number = 10 if source == 'generate' else 1
@@ -31,20 +30,22 @@ class MakePair:
             'left': ['front', 'back'],
             'right': ['front', 'back'],
         }
-        # f 0-> l 1-> b 1-> r 0->
         # f(0,1), b(2,3), l(4,5), r(6,7)
-        # [0,3,5,6]
+        # f 0-> l 1-> b 1-> r 0->
         # f 1-> r 1-> b 0-> l 0->
-        # [1,7,2,4]
-        # self.pair_idx_list = [[0, 3, 5, 6], [1, 7, 2, 4]]
-        self.pair_idx_list = [[0, 3, 5, 6], [1, 2, 4, 7]]
-
-    def pert_to_ori(self):
-        pass
+        # fblr (f0,l0), (b0,l1), (r0,f1), (r1,b1)
+        #   => (f1,r0), (b0,l1), (l0,f0), (r1,b1)
+        #   or (f0,l0), (b1,r1), (l1,b0), (r0,f1)
+        pair_idx_order1 = [1,6,2,5,4,0,7,3]
+        pair_idx_order2 = [0,4,3,7,5,2,6,1]
+        self.pair_idx_order = pair_idx_order2
 
     def pert_to_pert_two_camera(self):
         for cam1 in self.cam_list:
-            fw = open(self.pair_dir + f'/{cam1}_{self.sv_txt_name_suff}', 'a+')
+            cam_pair_path = self.pair_dir + f'/{cam1}_{self.sv_txt_name_suff}'
+            if os.path.exists(cam_pair_path):
+                os.remove(cam_pair_path)
+            fw = open(cam_pair_path, 'a+')
             iter_max = len(self.name_list)
             k = 0
             with tqdm(total=len(self.name_list)) as t:
@@ -74,11 +75,11 @@ class MakePair:
                     # ipdb.set_trace()
             fw.close()
 
-    def ori_to_ori(self):
-        pass
-
     def pert_to_pert_four_camera(self):
-        fw_all = open(f'{self.pair_dir}/{self.sv_txt_name_fblr}', 'a+')
+        fblr_pair_path = f'{self.pair_dir}/all_fblr_pair_names_list.txt'
+        if os.path.exists(fblr_pair_path):
+            os.remove(fblr_pair_path)
+        fw_all = open(fblr_pair_path, 'a+')
         iter_max = len(self.name_list)
         k = 0
         with tqdm(total=iter_max) as t:
@@ -92,31 +93,37 @@ class MakePair:
                     name2 = name2.replace(pidx, f'p{j:04d}')
                     sample_pair.append(f'{cam}/{name2}_0.jpg')
                     sample_pair.append(f'{cam}/{name2}_1.jpg')
-                for i in range(2):
-                    pair_str = []
-                    for idx in self.pair_idx_list[i]:
-                        pair_str.append(sample_pair[idx])
-                    suff = '\n'
-                    if k == iter_max - 1 and i == 1:
-                        suff = ''
-                    fw_all.write(' '.join(pair_str) + suff)
-                    # print(pair_str)
-                    # ipdb.set_trace()
+                pair_str = []
+                for idx in self.pair_idx_order:
+                    pair_str.append(sample_pair[idx])
+                suff = '\n'
+                if k == iter_max - 1:
+                    suff = ''
+                fw_all.write(' '.join(pair_str) + suff)
+                # print(pair_str)
+                # ipdb.set_trace()
                 t.set_description()
                 t.update()
                 k += 1
         fw_all.close()
+        
+    def pert_to_ori(self):
+        pass
+    
+    def ori_to_ori(self):
+        pass
+    
 
 if __name__ == '__main__':
 
     mode_list = ['train', 'test']
-    source_list = ['bev', 'generate']
-    # source_list = ['generate']
+    # source_list = ['bev', 'generate']
+    source_list = ['generate']
     # source_list = ['bev']
 
     for mode in mode_list:
         for source in source_list:
             print(mode, source)
             mp = MakePair(mode, source)
-            mp.pert_to_pert_two_camera()
+            # mp.pert_to_pert_two_camera()
             mp.pert_to_pert_four_camera()
